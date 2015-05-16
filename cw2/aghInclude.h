0c2b2ddf17929e1ac79e35bcb651841922c3d644
Jan Pogocki <jan.pogocki@gmail.com>
2015-05-16 13:55:23 +0200

    complex
    niedużo, ale coś...

--- cw2/aghInclude.h
+++ cw2/aghInclude.h
@@ -22,13 +22,13 @@ public:
 		n = _n;
 		m = _m;
 		matrix = new  T * [n];
-		
+
 		for (int i = 0; i < n; i ++)
 		{
 			matrix[i] = new T[m];
 		}
-		
-		
+
+
 	}
 
 	void setItem(int n, int m, T item) // n - wiersz, m - kolumna
@@ -64,7 +64,7 @@ public:
 			{
 				temp[i][j] = (matrix[i][j] + other.matrix[i][j]);
 			}
-			
+
 		}
 
 		return temp;
@@ -76,7 +76,7 @@ public:
 			{
 					for (int j = 0; j < m; j ++)
 					{
-					
+
 					}
 			}
 		}
@@ -84,7 +84,130 @@ public:
 
 };
 
+//------------------------------------------------------------------
+//KLASA aghComplex
 
+class aghComplex
+{
+  private:
+    double real, imaginary, r, i;
+  public:
+    aghComplex();
+    aghComplex(double _r, double _i);
+    aghComplex add(aghComplex number);
+    aghComplex sub(aghComplex number);
+    aghComplex mul(aghComplex number);
+    aghComplex div(aghComplex number);
+    aghComplex power(int wykladnik);
+    double re();
+    double im();
+    void print();
+};
+
+aghComplex::aghComplex()
+{
+    real = imaginary = 0;
+}
+
+aghComplex::aghComplex(double _r, double _i)
+{
+    real = r = _r;
+    imaginary = i = _i;
+}
+
+aghComplex aghComplex::add(aghComplex number)
+{
+    r = real+number.real;
+    i = imaginary+number.imaginary;
+    return aghComplex(r,i);
+}
+
+aghComplex aghComplex::sub(aghComplex number)
+{
+    r = real - number.real;
+    i = imaginary - number.imaginary;
+    return aghComplex(r,i);
+}
+
+aghComplex aghComplex::mul(aghComplex number)
+{
+    r = real*number.real - imaginary*number.imaginary;
+    i = real*number.imaginary + imaginary*number.real;
+    return aghComplex(r,i);
+}
+
+aghComplex aghComplex::div(aghComplex number)
+{
+    r = (real*number.real + imaginary*number.imaginary)/(number.real*number.real+number.imaginary*number.imaginary);
+    i = (imaginary*number.real - real*number.imaginary)/(number.real*number.real+number.imaginary*number.imaginary);
+    return aghComplex(r,i);
+}
+
+aghComplex aghComplex::power(int wykladnik)
+{
+    aghComplex number;
+    number = aghComplex(real, imaginary);
+
+    if (wykladnik == 0)
+    {
+        r = 1;
+        i = 0;
+    }
+    else if (wykladnik > 1)
+    {
+        aghComplex temp;
+        temp = number;
+
+        for (int j=0; j<wykladnik-1; j++)
+        {
+            temp.mul(number);
+            temp = aghComplex(temp.r, temp.i);
+        }
+
+        r = temp.r;
+        i = temp.i;
+    }
+    else if (wykladnik < 0)
+    {
+        //transformacja na postac trygonometryczna
+        double modul, fi;
+        modul = sqrt(real*real+imaginary*imaginary);
+
+        if (real > 0)
+            fi = atan(imaginary/real);
+        else if (real < 0)
+            fi = atan(imaginary/real)+M_PI;
+
+        r = pow(modul, wykladnik)*cos(wykladnik*fi);
+        i = pow(modul, wykladnik)*sin(wykladnik*fi);
+    }
+    return aghComplex(r,i);
+}
+
+double aghComplex::re()
+{
+    return r;
+}
+
+double aghComplex::im()
+{
+    return i;
+}
+
+void aghComplex::print()
+{
+    if (re() == 0 && im() != 0)
+        cout << im() << "i";
+    else if (re() != 0 && im() == 0)
+        cout << re();
+    else if (re() == 0 && im() == 0)
+        cout << "0";
+    else
+    {
+        cout << re();
+        cout << std::showpos << im() << "i" << std::noshowpos;
+    }
+}
 
 
 #endif
